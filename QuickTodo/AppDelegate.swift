45701f271a6555844f5ec0d46b44960e0417bfd5
Arthur Kazemi <bidadh@gmail.com>
2017-04-15 13:43:39 +1000

    complete scenes

--- QuickTodo/AppDelegate.swift
+++ QuickTodo/AppDelegate.swift
@@ -24,9 +24,16 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-  var window: UIWindow?
+    var window: UIWindow?
 
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
-    return true
-  }
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil) -> Bool {
+        let service = TaskService()
+        let sceneCoordinator = SceneCoordinator(window: window!)
+
+        let tasksViewModel = TasksViewModel(taskService: service, coordinator: sceneCoordinator)
+        let firstScene = Scene.tasks(tasksViewModel)
+        sceneCoordinator.transition(to: firstScene, type: .root)
+
+        return true
+    }
 }
