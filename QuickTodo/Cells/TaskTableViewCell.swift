9e03c0a76d882baec9f2f634d04c99da3efee27f
Arthur Kazemi <bidadh@gmail.com>
2017-04-15 14:34:35 +1000

    complete cell

--- QuickTodo/Cells/TaskTableViewCell.swift
+++ QuickTodo/Cells/TaskTableViewCell.swift
@@ -26,12 +26,29 @@ import RxSwift
 
 class TaskItemTableViewCell: UITableViewCell {
 
-  @IBOutlet var title: UILabel!
-  @IBOutlet var button: UIButton!
-  var disposeBag = DisposeBag()
+    @IBOutlet var title: UILabel!
+    @IBOutlet var button: UIButton!
+    var disposeBag = DisposeBag()
 
-  func configure(with item: TaskItem, action: CocoaAction) {
-    
-  }
+    func configure(with item: TaskItem, action: CocoaAction) {
+        button.rx.action = action
 
+        item.rx.observe(String.self, "title")
+                .subscribe(onNext: { [weak self] title in
+                    self?.title.text = title
+                })
+                .addDisposableTo(disposeBag)
+        item.rx.observe(Date.self, "checked")
+                .subscribe(onNext: { [weak self] date in
+                    let image = UIImage(named: (date == nil) ? "ItemNotChecked" : "ItemChecked")
+                    self?.button.setImage(image, for: .normal)
+                })
+                .addDisposableTo(disposeBag)
+    }
+
+    override func prepareForReuse() {
+        button.rx.action = nil
+        disposeBag = DisposeBag()
+        super.prepareForReuse()
+    }
 }
