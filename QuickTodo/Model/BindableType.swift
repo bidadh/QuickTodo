aa6937d65f60340a3a83adff4f476fca535ff4bf
Arthur Kazemi <bidadh@gmail.com>
2017-04-15 12:11:37 +1000

    complete TaskServiceType, TaskService, BindableType, etc

--- QuickTodo/Model/BindableType.swift
+++ QuickTodo/Model/BindableType.swift
@@ -23,3 +23,16 @@
 import UIKit
 import RxSwift
 
+protocol BindableType {
+    associatedtype ViewModelType
+    var viewModel: ViewModelType! { get set }
+    func bindViewModel()
+}
+
+extension BindableType where Self: UIViewController {
+    mutating func bindViewModel(to model: Self.ViewModelType) {
+        viewModel = model
+        loadViewIfNeeded()
+        bindViewModel()
+    }
+}
--- QuickTodo/Model/TaskItem.swift
+++ QuickTodo/Model/TaskItem.swift
@@ -24,4 +24,13 @@ import Foundation
 import RealmSwift
 import RxDataSources
 
+class TaskItem: Object {
+    dynamic var uid: Int = 0
+    dynamic var title: String = ""
+    dynamic var added: Date = Date()
+    dynamic var checked: Date? = nil
+    override class func primaryKey() -> String? {
+        return "uid"
+    }
+}
 
